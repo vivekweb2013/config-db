8d9a76ca859322b85aaa5f531806e039c1708bb9
John Goh <john@flanksource.com>
2022-04-25 14:18:26 +0800

    fix: remove unnecessary code chunk

--- db/migrations/6_tags_column.sql
+++ db/migrations/6_tags_column.sql
@@ -2,10 +2,6 @@
 -- +goose StatementBegin
 ---
 
-ALTER TABLE config_item  ALTER COLUMN config  SET DATA TYPE jsonb USING config::jsonb;
-ALTER TABLE config_scraper  ALTER COLUMN spec  SET DATA TYPE jsonb USING spec::jsonb;
-ALTER TABLE config_change  ALTER COLUMN patches  SET DATA TYPE jsonb USING patches::jsonb;
-ALTER TABLE config_analysis  ALTER COLUMN analysis  SET DATA TYPE jsonb USING analysis::jsonb;
 ALTER TABLE config_item  ADD tags jsonb null;
 
 -- +goose StatementEnd
